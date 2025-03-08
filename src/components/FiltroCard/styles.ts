cc7214afb951be876e476942c6021242930857ea
Luis Henrique <luish7572@gmail.com>
2025-03-07 21:58:43 -0300

    ajustando barra lateral

---
+++ src/components/FiltroCard/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+import { Props } from './index'
+
+export const Card = styled.div<Props>`
+  padding: 8px;
+  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#a1a1a1')};
+  border-radius: 8px;
+  background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
+  color: ${(props) => (props.ativo ? '#1E90FF' : '#5e5e5e')};
+  display: flex;
+  flex-direction: column;
+`
+
+export const Contador = styled.span`
+  font-weight: bold;
+  font-size: 24px;
+  display: block;
+`
+
+export const Label = styled.span`
+  font-size: 14px;
+`
---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,20 @@
+import { FiltroCard } from '../../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <input type="text" placeholder="buscar" />
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
+
+export default BarraLateral
